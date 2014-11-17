b08e2ac03a897e052ce80388758e969eab8e9593
jeppedl <jdl@jdl.lan>
2014-11-17 14:45:58 +0100

    Tiny update
    New variable and smaller limit

--- commenttest.py
+++ commenttest.py
@@ -3,6 +3,6 @@ r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
 
 
 subreddit = r.get_subreddit('videos')
-#subreddit_comments = subreddit.get_comments()
-for comment in subreddit.get_comments(limit=100):
+subreddit_comments = subreddit.get_comments(limit=10)
+for comment in subreddit_comments:
 	print (comment.body)
