52ecf501ffb7352344537a391f91584841b3a4f3
jeppedl <jdl@jdl.lan>
2014-11-17 13:42:10 +0100

    Updated

--- reddittest.py
+++ reddittest.py
@@ -1,6 +1,7 @@
-import praw
 
+
+import praw
 r = praw.Reddit(user_agent='my_cool_application')
 
-submissions = r.get_subreddit('opensource').get_hot(limit=5)
-[str(x) for x in submissions]
+submissions = r.get_subreddit('videos').get_hot(limit=5)
+print([str(x) for x in submissions])
