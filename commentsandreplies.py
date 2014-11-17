c19310aba3b527ccf8dcccfb00d277978451343f
jeppedl <jdl@jdl.lan>
2014-11-17 16:47:56 +0100

    Still not working

--- commentsandreplies.py
+++ commentsandreplies.py
@@ -12,52 +12,9 @@ r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
 subreddit = r.get_subreddit('cringepics')
 #subreddit = r.get_subreddit('askscience')
 subreddit_comments = subreddit.get_comments(limit=1)
-subreddit_replies = subreddit.get_comments.replies(limit=2)
-myList = []
-repList = []
-
-for comment in subreddit_comments:
-	myList.append(comment.body)
-	output_file.writerow([comment.body.encode('utf-8')])
-
-for comment in subreddit_replies:
-	repList.append(comment.replies)
-	#output_file.writerow([comment.replies.encode('utf-8')])
-
-wordlist = []
-with open('word_list.txt') as f:
-  for line in f:
-    wordlist.append(line.split('\t'))
-
-dict = {}
-for i in range(0,len(wordlist)):
-  dict[wordlist[i][0]] = float(wordlist[i][1])
-keywords = dict.viewkeys()
-
-L = len(myList)
-
-words=0
-i=0
-profcount =0
-score=0
-while i< L:
-  redcomment= myList[i]
-  if(profanity.contains_profanity(myList[i])==True):
-  	profcount +=1
-    
-  for word in redcomment.split():
-     word = word.lower()
-     if word in keywords:
-       score += dict[word]
-       words +=1
-
-  i+=1
-
-print profcount
-print "The average sentiment value is " + str(score/words)
-print repList
 
 
-#for c in myList:
-#	print c
+myList[]
+myList.append(subreddit_comments) 
 
+print myList
