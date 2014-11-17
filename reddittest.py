05883d29c5f662eca6d1db443cb09ddc3908683a
Niclas Bach Nielsen <niclas.bach@gmail.com>
2014-11-17 13:52:56 +0100

    Changed user agent.
    More descriptive and includes a shared reddit user

--- reddittest.py
+++ reddittest.py
@@ -1,7 +1,7 @@
 
 
 import praw
-r = praw.Reddit(user_agent='my_cool_application')
+r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
 
 submissions = r.get_subreddit('videos').get_hot(limit=5)
 print([str(x) for x in submissions])
