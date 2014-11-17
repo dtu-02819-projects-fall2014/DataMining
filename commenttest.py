1931e766aba821155c80473106bfeb43a6b35f83
jeppedl <jdl@jdl.lan>
2014-11-17 14:44:09 +0100

    Prints the last 100 comments from a subreddit

--- commenttest.py
+++ commenttest.py
@@ -1,7 +1,8 @@
 import praw
 r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
 
-comments = r.get_content("http://www.reddit.com/r/%s/comments" % subreddit, limit = 100)
 
-
-print(submission)
+subreddit = r.get_subreddit('videos')
+#subreddit_comments = subreddit.get_comments()
+for comment in subreddit.get_comments(limit=100):
+	print (comment.body)
