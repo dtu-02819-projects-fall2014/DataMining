6bd810a631307548eea243e0bf1165befcd869c3
jeppedl <jdl@jdl.lan>
2014-11-19 14:28:07 +0100

    Now works and exports comments to csv file

--- commentsandreplies.py
+++ commentsandreplies.py
@@ -1,4 +1,10 @@
 import praw
+import csv
+    
+
+
+output_file = csv.writer(open("replies.csv", "w",0), dialect='excel')
+output_file.writerow(["comment"])
 
 r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
 
@@ -7,8 +13,10 @@ hotSubmissions = subreddit.get_hot(limit=20)
 
 def getReplies(comments):
   for comment in comments:
-    print comment.body
+    #print comment.body
+    output_file.writerow([comment.body.encode('utf-8')])
     getReplies(comment.replies)
+    
 
 for submission in hotSubmissions:
   submission.replace_more_comments(limit=None, threshold=0)
