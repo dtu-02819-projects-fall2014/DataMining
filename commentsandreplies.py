ec73dcf58d5c8b96976cd285c456e5652638a33d
jeppedl <jdl@jdl.lan>
2014-11-19 14:18:32 +0100

    WORKS!
    Can now get comments up to 10 layers down. Problem when comments
    include emoticons.

--- commentsandreplies.py
+++ commentsandreplies.py
@@ -1,20 +1,15 @@
 import praw
-import csv
-from profanity import profanity
-    
-
-
-output_file = csv.writer(open("comments.csv", "w",0), dialect='excel')
-output_file.writerow(["comment"])
 
 r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
 
-subreddit = r.get_subreddit('cringepics')
-#subreddit = r.get_subreddit('askscience')
-subreddit_comments = subreddit.get_comments(limit=1)
+subreddit = r.get_subreddit('videos')
+hotSubmissions = subreddit.get_hot(limit=20)
 
+def getReplies(comments):
+  for comment in comments:
+    print comment.body
+    getReplies(comment.replies)
 
-myList[]
-myList.append(subreddit_comments) 
-
-print myList
+for submission in hotSubmissions:
+  submission.replace_more_comments(limit=None, threshold=0)
+  getReplies(submission.comments)
