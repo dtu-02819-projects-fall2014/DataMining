452b9f25514c7743f6b713c46998260d80a4f545
jeppedl <jdl@jdl.local>
2014-11-27 15:02:23 +0100

    Clean up

--- reddittest.py
+++
@@ -1,7 +0,0 @@
-
-
-import praw
-r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
-
-submissions = r.get_subreddit('videos').get_hot(limit=5)
-print([str(x) for x in submissions])
