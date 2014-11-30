c2715389223af627c790e004ace6f5d74907bc4b
jeppedl <jdl@jdl.lan>
2014-11-30 22:35:23 +0100

    AFINN-111 implementation
    The AFINN-111.txt is now implemented in sentiment_analysis2.py and
    works like a charm. Also overall changes (variable names, indents,
    etc.) has been made.

--- sentiment_analysis2.py
+++ sentiment_analysis2.py
@@ -1,8 +1,15 @@
 import praw
 import csv
 import os
+import math
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 SEMANTIC_RED = "reddit_semantic.csv"
+AFINN_FILE = 'AFINN-111.txt'
+
 r = praw.Reddit(user_agent='Sentiment analysis of subreddits by /u/langeniels')
 subredInCsv = True
 
@@ -21,42 +28,45 @@ def semantic_reddit(subreddit='all', comment_amount=200):
 
     subreddit_name = r.get_subreddit(subreddit)
     subreddit_comments = subreddit_name.get_comments(limit=comment_amount)
-    myList = []
+    
+    subreddit_comments_list = []
     for comment in subreddit_comments:
-        myList.append(comment.body)
+        subreddit_comments_list.append(comment.body)
 
-    wordlist = []
-    with open('word_list.txt') as f:
-        for line in f:
-            wordlist.append(line.split('\t'))
-
-    profanityList = []
+    profanity_list = []
     with open('profanity-list-google.txt') as swearword:
         for someword in swearword:
             someword = someword.strip()
-            profanityList.append(someword)
+            profanity_list.append(someword)
 
-    wordDict = {}
-    for i in range(0, len(wordlist)):
-        wordDict[wordlist[i][0]] = float(wordlist[i][1])
-    keywords = wordDict.viewkeys()
+    word_count = 0
+    profanity_count = 0
 
-    words = 0
-    profcount = 0
-    score = 0
-
-    for redcomment in myList:
+    for redcomment in subreddit_comments_list:
         redcomment = redcomment.encode('utf-8')
+        for word in redcomment.lower().split():
+            word_count += 1
+            if word in profanity_list:
+            	profanity_count += 1
 
-    for word in redcomment.split():
-        if word in profanityList:
-            profcount += 1
-        if word in keywords:
-            score += wordDict[word]
-            words += 1
-    i += 1
+    afinn = dict(map(lambda (w, s): (w, int(s)), [ 
+            ws.strip().split('\t') for ws in open(AFINN_FILE) ]))
 
-    average_sentiment = str(score/words)
+    afinn_score = 0
+
+    string_comments = ''.join(subreddit_comments_list)
+
+    afinn_score = map(lambda word: afinn.get(word, 0), string_comments.lower().split())
+    
+    if afinn_score:
+        #sentiment_score = float(sum(afinn_score))/math.sqrt(len(afinn_score))
+        sentiment_score = float(sum(afinn_score))/len(afinn_score)
+    else:
+        sentiment_score = 0
+
+    print "profanity count: " + str(profanity_count)
+    print "total words: " + str(word_count)
+    print "Afinn sentiment avg: " + str(sentiment_score)
 
     with open(SEMANTIC_RED, 'ab') as sm, open(SEMANTIC_RED, 'rt') as f:
         reader = csv.reader(f, delimiter=',')
@@ -67,9 +77,9 @@ def semantic_reddit(subreddit='all', comment_amount=200):
             else:
                 subredInCsv = True
         if subredInCsv is False:
-            writer.writerow([subreddit, average_sentiment])
+            writer.writerow([subreddit, sentiment_score])
         else:
             print "You have already fetched this subreddit."
             print "Try a different."
 
-semantic_reddit('funny', 200)
+semantic_reddit('christianity', 200)
